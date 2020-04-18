b73584fe07c39cf23696349c236bc876b49d60da
Brian Racca <bracca@tekgenesis.com>
2020-04-18 20:34:09 -0300

    add heroku dependencies

---
+++ demo/src/main/java/com/mutant/exercise/api/DNAStatsController.java
@@ -0,0 +1,17 @@
+package com.mutant.exercise.api;
+
+import com.mutant.exercise.model.DNA;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequestMapping("/stats")
+@RestController
+public class DNAStatsController {
+
+}
---
+++ demo/src/main/java/com/mutant/exercise/services/DNAStatsService.java
@@ -0,0 +1,12 @@
+package com.mutant.exercise.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DNAStatsService {
+    private static final Logger log = LoggerFactory.getLogger(DNAStatsService.class);
+
+
+}
